cdf2f6ad8732c14062e2af7f67cd74e2c6c512ff
bingyue <bingyue56@163.com>
2016-04-16 00:51:18 +0800

    调整demo

---
+++ bingyue/rmi/Client.java
@@ -0,0 +1,20 @@
+package bingyue.rmi;
+
+import java.rmi.Naming;
+
+/**
+ * @author BingYue
+ */
+public class Client {
+	
+	public static void main(String[] args){
+		try {
+			//调用远程对象，注意RMI路径与接口必须与服务器配置一致
+			SearchService searchService=(SearchService)Naming.lookup("rmi://127.0.0.1:5678/searchService");
+			User user=searchService.findUser("100");
+			System.out.println(user.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ bingyue/rmi/SearchServiceImpl.java
@@ -0,0 +1,24 @@
+package bingyue.rmi;
+
+import java.rmi.RemoteException;
+
+public class SearchServiceImpl implements SearchService{
+
+	/**
+	 * 抛出RemoteException
+	 * @throws RemoteException
+	 */
+	public SearchServiceImpl() throws RemoteException {
+		super();
+		}
+	
+	@Override
+	public User findUser(String id) throws RemoteException {
+		/**
+		 * 模拟查找返回数据
+		 */
+		User user=new User(id,"Tom","18岁");
+		return user;
+	}
+
+}
