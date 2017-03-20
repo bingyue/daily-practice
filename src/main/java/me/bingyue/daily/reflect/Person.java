738e5c00ce3414a0696ada4d2b2e4290871bf13c
danielbing <danielbing@sohu-inc.com>
2017-03-20 10:45:33 +0800

    反射练习

---
+++ src/main/java/me/bingyue/daily/reflect/Person.java
@@ -0,0 +1,11 @@
+package me.bingyue.daily.reflect;
+
+public class Person {
+
+	public static String NOON="上午好";
+	private static String EVENING="晚上好";
+	
+	public void speak(String content){
+		System.out.println(content);
+	}
+}
